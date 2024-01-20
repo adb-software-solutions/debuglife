c19d5e50b70f1576084d6a54ab929d692397f01a
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2024-01-20 04:10:23 +0000

    refactor: Run linters on auth code.

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,10 @@
-from knox.views import LoginView as KnoxLoginView
+from knox.views import LoginView as KnoxLoginView, LogoutView as KnoxLogoutView, LogoutAllView as KnoxLogoutAllView
+
 from rest_framework.authentication import SessionAuthentication
+
+from knox.serializers import UserSerializer
 
 
 class LoginView(KnoxLoginView):
     authentication_classes = [SessionAuthentication]
+    serializer_class = UserSerializer

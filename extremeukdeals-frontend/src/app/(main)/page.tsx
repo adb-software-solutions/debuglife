7dacb6c0d823b436bdac9f8d94c0af84a7aaee69
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2024-02-03 18:10:20 +0000

    frontend: Try and generate url dynamically.

--- extremeukdeals-frontend/src/app/(main)/page.tsx
+++ extremeukdeals-frontend/src/app/(main)/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/common/frontend/NavBar";
 import FooterComponent from "@/components/common/frontend/footer/Footer";
 import HeroSectionComponent from "@/components/frontend/home/HeroSection";
 import LatestDealsSection from "@/components/frontend/home/LatestDealsSection";
-import { footerNavigation, footerSocial } from "@/fixtures/footerData";
+import {footerNavigation, footerSocial} from "@/fixtures/footerData";
 
 const navigation = [
     {title: "Home", href: "/", current: true},
@@ -11,12 +11,29 @@ const navigation = [
     {title: "Community", href: "/community", current: false},
 ];
 
+function getBaseUrl() {
+    // Check if running on the server
+    if (typeof window === "undefined") {
+        // Running on server - use Docker container name
+        return "http://extremeukdeals-backend:8000";
+    } else {
+        // Running on client - use specific domain
+        return "https://extremeukdeals.com";
+    }
+}
+
+// Function to get the full API URL by appending the API path
+function getApiUrl(apiPath: string): string {
+    const baseUrl = getBaseUrl(); // Get the base URL dynamically
+    return `${baseUrl}${apiPath}`; // Construct the full API URL
+}
+
 async function getProducts() {
     const res = await fetch(
         "http://extremeukdeals-backend:8000/api/shop/affiliate_products/",
         {
             cache: "no-cache",
-        }
+        },
     );
     const products = await res.json();
     return products.results;
@@ -29,11 +46,14 @@ export default async function Home() {
         <div>
             <Navbar navigation={navigation} />
 
-            <HeroSectionComponent/>
+            <HeroSectionComponent />
 
-            <LatestDealsSection products={products}/>
+            <LatestDealsSection products={products} />
 
-            <FooterComponent navigation={footerNavigation} social={footerSocial}/>
+            <FooterComponent
+                navigation={footerNavigation}
+                social={footerSocial}
+            />
         </div>
     );
 }

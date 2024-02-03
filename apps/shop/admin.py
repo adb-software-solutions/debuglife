b1285a032d633ef8616e867b17bbd1c24a055be9
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2024-02-03 04:37:48 +0000

    backend: Add product category to admin portal.

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AffiliateProduct, AffiliateProgram, PinterestBoard, PinterestPin
+from .models import AffiliateProduct, AffiliateProgram, AffiliateCategory, PinterestBoard, PinterestPin
 
 
 class AffiliateProgramAdmin(admin.ModelAdmin[AffiliateProgram]):
@@ -20,6 +20,10 @@ class AffiliateProductAdmin(admin.ModelAdmin[AffiliateProduct]):
     ordering = ("product_name", "product_price", "affiliate_program")
     sortable_by = ("product_name", "product_price", "affiliate_program")
 
+
+class AffiliateCategoryAdmin(admin.ModelAdmin[AffiliateCategory]):
+    list_display = ("category_name", "amazon_category_id")
+    ordering = ("category_name",)
 
 class PinterestBoardAdmin(admin.ModelAdmin[PinterestBoard]):
     list_display = ("board_name", "id")
@@ -41,5 +45,6 @@ class PinterestPinAdmin(admin.ModelAdmin[PinterestPin]):
 
 admin.site.register(AffiliateProgram, AffiliateProgramAdmin)
 admin.site.register(AffiliateProduct, AffiliateProductAdmin)
+admin.site.register(AffiliateCategory, AffiliateCategoryAdmin)
 admin.site.register(PinterestBoard, PinterestBoardAdmin)
 admin.site.register(PinterestPin, PinterestPinAdmin)

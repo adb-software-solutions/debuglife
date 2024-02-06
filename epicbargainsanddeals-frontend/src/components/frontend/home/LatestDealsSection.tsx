464ded682ef39871cb6cb992c9c1d6f32822a200
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2024-02-06 02:04:43 +0000

    frontend: Display product price and previous price.

--- epicbargainsanddeals-frontend/src/components/frontend/home/LatestDealsSection.tsx
+++ epicbargainsanddeals-frontend/src/components/frontend/home/LatestDealsSection.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
-import { ProductPropsType } from "@/types/product";
+import {ProductPropsType} from "@/types/product";
 
-export default function LatestDealsSection({products}: ProductPropsType ) {
+export default function LatestDealsSection({products}: ProductPropsType) {
     return (
         <div className="bg-white">
-            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">
                 <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
                     {products.map((product) => (
-                        <div key={product.id} className="grid grid-rows-[auto,1fr,auto] h-full">
+                        <div
+                            key={product.id}
+                            className="grid h-full grid-rows-[auto,1fr,auto]"
+                        >
                             <div className="relative h-72 w-full overflow-hidden rounded-lg">
                                 <Image
                                     src={product.product_image}
@@ -21,11 +24,18 @@ export default function LatestDealsSection({products}: ProductPropsType ) {
                                 <h3 className="text-sm font-medium text-gray-900">
                                     {product.product_name}
                                 </h3>
+                                {/* Price and Previous Price Display */}
+                                <p className="text-lg font-semibold py-2 text-gray-900">
+                                    ${product.product_price}{" "}
+                                    <span className="text-sm font-normal text-gray-500 line-through">
+                                        ${product.previous_price}
+                                    </span>
+                                </p>
                             </div>
-                            <div className="mt-6">
+                            <div className="mt-2">
                                 <a
                                     href={product.affiliate_link}
-                                    className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
+                                    className="bg-brand hover:bg-brand relative flex items-center justify-center rounded-md border border-transparent px-8 py-2 text-sm font-medium text-gray-900"
                                 >
                                     Buy Now
                                 </a>
@@ -37,4 +47,3 @@ export default function LatestDealsSection({products}: ProductPropsType ) {
         </div>
     );
 }
-
--- epicbargainsanddeals-frontend/src/types/product.ts
+++ epicbargainsanddeals-frontend/src/types/product.ts
@@ -2,6 +2,8 @@ export type ProductType = {
     id: string;
     product_name: string;
     product_image: string;
+    product_price: string;
+    previous_price: string;
     affiliate_link: string;
 };
 

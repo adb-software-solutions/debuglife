c5debb10a2adbecd97a4df4a69ab7ae18b8bb2ac
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2024-02-06 02:26:55 +0000

    frontend: Add recent deals page.

---
+++ epicbargainsanddeals-frontend/src/app/(main)/recent-deals/page.tsx
@@ -0,0 +1,29 @@
+import HeroSectionComponent from "@/components/frontend/latest-deals/HeroSection";
+import LatestDealsSection from "@/components/frontend/latest-deals/LatestDealsSection";
+import { getApiUrl } from "@/lib/getApi";
+
+async function getProducts() {
+    const res = await fetch(
+        getApiUrl("/api/shop/affiliate_products"),
+        {
+            next: {
+                revalidate: 600,
+            }
+        },
+    );
+    const products = await res.json();
+    return products.results;
+}
+
+export default async function RecentDealsPage() {
+    const products = await getProducts();
+
+    return (
+        <>
+            <HeroSectionComponent />
+            <LatestDealsSection products={products} />
+        </>
+    );
+}
+
+export const dynamic = "force-dynamic";

1bdf4b14e050b0ae8def3848eb71da3cce54b140
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2024-02-06 02:42:11 +0000

    frontend: Add deal categories page.

---
+++ epicbargainsanddeals-frontend/src/app/(main)/deal-categories/page.tsx
@@ -0,0 +1,29 @@
+import HeroSectionComponent from "@/components/frontend/latest-deals/HeroSection";
+import CategoriesSection from "@/components/frontend/deal-categories/CategoriesSection";
+import { getApiUrl } from "@/lib/getApi";
+
+async function getCategories() {
+    const res = await fetch(
+        getApiUrl("/api/shop/affiliate_categories"),
+        {
+            next: {
+                revalidate: 600,
+            }
+        },
+    );
+    const categories = await res.json();
+    return categories.results;
+}
+
+export default async function RecentDealsPage() {
+    const categories = await getCategories();
+
+    return (
+        <>
+            <HeroSectionComponent />
+            <CategoriesSection categories={categories} />
+        </>
+    );
+}
+
+export const dynamic = "force-dynamic";
---
+++ epicbargainsanddeals-frontend/src/app/eapi/shop/categories/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getApiUrl } from "@/lib/getApi";
+
+async function fetchCategories() {
+    const url = getApiUrl(`/api/shop/affiliate_categories`);
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+export async function GET(req: NextRequest) {
+    const data = await fetchCategories();
+    return NextResponse.json(data);
+}

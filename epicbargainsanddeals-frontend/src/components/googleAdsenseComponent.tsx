ba0a97c179b6b1a4e15bce3592558438f69be6c9
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2024-02-07 04:15:09 +0000

    frontend: Update google adsense account details.

--- epicbargainsanddeals-frontend/src/components/googleAdsenseComponent.tsx
+++ epicbargainsanddeals-frontend/src/components/googleAdsenseComponent.tsx
@@ -4,7 +4,7 @@ export function GoogleAdsenseComponent() {
     return (
         <Script
             async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6574915306585372"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2415546728741367"
             crossOrigin="anonymous"
             strategy="lazyOnload"
         />

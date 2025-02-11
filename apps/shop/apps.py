2e23069c0d656a8a78bf48c1b198f718bb637f0e
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-02-11 03:51:12 +0000

    refactor: Delete unneeded files.

--- apps/shop/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.shop"
-
-    def ready(self) -> None:
-        from apps.shop.scheme import KnoxTokenScheme as KnoxTokenScheme  # noqa
--- authentication/views.py
+++
@@ -1,8 +0,0 @@
-from knox.serializers import UserSerializer
-from knox.views import LoginView as KnoxLoginView
-from rest_framework.authentication import SessionAuthentication
-
-
-class LoginView(KnoxLoginView):
-    authentication_classes = [SessionAuthentication]
-    serializer_class = UserSerializer

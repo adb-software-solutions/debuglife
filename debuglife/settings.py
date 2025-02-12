cba558875221a65b43a2ba6b421796d59adcd56d
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-02-12 02:47:23 +0000

    backend: Update all the django settings.

--- debuglife/settings.py
+++ debuglife/settings.py
@@ -12,27 +12,41 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import logging
 import os
+import urllib.parse
 from pathlib import Path
 from typing import List
 
 import django_stubs_ext
+# import sentry_sdk
+from django.templatetags.static import static
+
+logger = logging.getLogger(__name__)
+
+django_stubs_ext.monkeypatch()
+
+#SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(int(os.environ.get("DEBUG", "0")))
+DEBUG_TOOLBAR_ENABLED = bool(int(os.environ.get("DEBUG_TOOLBAR_ENABLED", "0")))
+
+# if not DEBUG:
+#     sentry_sdk.init(
+#         dsn="https://2c8e205f6fb07f977ea8d859ad70df1b@o4507589315133440.ingest.de.sentry.io/4507589320441936",
+#         # Set traces_sample_rate to 1.0 to capture 100%
+#         # of transactions for performance monitoring.
+#         traces_sample_rate=1.0,
+#         # Set profiles_sample_rate to 1.0 to profile 100%
+#         # of sampled transactions.
+#         # We recommend adjusting this value in production.
+#         profiles_sample_rate=1.0,
+#         environment=os.environ.get("SENTRY_ENVIRONMENT", "staging"),
+#     )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SITE_URL = os.environ.get("SITE_URL", "http://localhost:8000")
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
     "SECRET_KEY", "django-insecure-fdg2(_6j#^s=!x*$-t#f1q6y9bu-k-4i$xrsnuv%xn@xe_r(r2"
 )
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG", "0")))
-DEBUG_TOOLBAR_ENABLED = bool(int(os.environ.get("DEBUG_TOOLBAR_ENABLED", "0")))
 
 if DEBUG:
     # will output to your console
@@ -41,7 +55,19 @@ if DEBUG:
         format="%(asctime)s %(levelname)s %(message)s",
     )
 
-ALLOWED_HOSTS: List[str] = ["*"]
+SITE_DOMAIN = os.environ.get("SITE_DOMAIN", "localhost")
+
+FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:3000")
+
+if not SITE_DOMAIN:
+    raise ValueError("SITE_DOMAIN must be set.")
+
+if DEBUG:
+    ALLOWED_HOSTS: List[str] = ["*"]
+else:
+    ALLOWED_HOSTS = [
+        "api." + SITE_DOMAIN,
+    ]
 
 
 # Application definition
@@ -50,6 +76,7 @@ INSTALLED_APPS = [
     "apps.blog",
     "authentication",
     "corsheaders",
+    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -61,9 +88,20 @@ INSTALLED_APPS = [
 if DEBUG_TOOLBAR_ENABLED:
     INSTALLED_APPS += ["debug_toolbar"]
 
-AUTH_USER_MODEL = "authentication.User"
+UNFOLD = {
+    "SITE_TITLE": "DebugLife Admin",
+    "SITE_HEADER": "DebugLife Admin",
+    "SITE_ICON": {
+        "light": lambda request: static("logo.svg"),
+        "dark": lambda request: static("logo-dark.svg"),
+    },
+    "SITE_LOGO": {
+        "light": lambda request: static("logo.svg"),
+        "dark": lambda request: static("logo-dark.svg"),
+    },
+}
 
-REST_KNOX = {"AUTH_HEADER_PREFIX": "Bearer", "TOKEN_TTL": None}
+AUTH_USER_MODEL = "authentication.User"
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
@@ -119,6 +157,16 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = bool(int(os.environ.get("EMAIL_USE_TLS", "1")))
+EMAIL_USE_SSL = bool(int(os.environ.get("EMAIL_USE_SSL", "0")))
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "")
+DEFAULT_FROM_EMAIL_NAME = os.environ.get("DEFAULT_FROM_EMAIL_NAME", "DebugLife")
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -169,9 +217,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
-DEFAULT_DOMAIN = "http://localhost:8000/"
-
-django_stubs_ext.monkeypatch()
+SITE_ID = 1
+SITE_NAME = "fliplytics"
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
@@ -183,11 +230,15 @@ CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 
+CELERY_ROUTES = {}
+
 # CORS
 
 CORS_ALLOWED_ORIGINS = [
-    "https://debuglife.dev",
-    "https://www.debuglife.dev",
+    "https://" + SITE_DOMAIN,
+    "https://api." + SITE_DOMAIN,
+    "ws://" + SITE_DOMAIN,
+    "ws://api." + SITE_DOMAIN,
 ]
 
 if DEBUG:
@@ -208,24 +259,37 @@ CORS_ALLOW_HEADERS = [
     "origin",
     "user-agent",
     "X-CSRFToken",
+    "sentry-trace",
+    "baggage",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
-SESSION_ENGINE = "authentication.redis_sessions.session"
-
-SESSION_REDIS = {
-    "host": "debuglife-redis",
-    "port": 6379,
-    "db": 0,
-    "password": "",
-    "prefix": "session",
-    "socket_timeout": 1,
-    "retry_on_timeout": False,
-}
-
-SESSION_COOKIE_SAMESITE = None
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_COOKIE_SAMESITE = "Lax"
 SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_AGE = 864000
+SESSION_COOKIE_DOMAIN = "." + SITE_DOMAIN
+
+SESSION_CACHE_ALIAS = "session"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("CACHE_BACKEND_URL", ""),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    },
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("SESSION_BACKEND_URL", ""),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    },
+}
 
 if DEBUG:
     SESSION_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SECURE = True

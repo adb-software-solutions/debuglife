4156ce61be63a5a90b3fcca120054ff50c650bd3
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-02-13 05:59:31 +0000

    frontend: Remove unneeded image config.

--- debuglife-frontend/next.config.js
+++ debuglife-frontend/next.config.js
@@ -3,18 +3,7 @@ const nextConfig = {
     reactStrictMode: false,
     images: {
         remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "m.media-amazon.com",
-                port: "",
-                pathname: "/images/I/**",
-            },
-            {
-                protocol: "https",
-                hostname: "images-na.ssl-images-amazon.com",
-                port: "",
-                pathname: "/images/I/**",
-            }
+            
         ],
     },
 };

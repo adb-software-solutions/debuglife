627b9bd10f97c034ba81ec81008dbef0c5b21076
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-02-23 22:55:29 +0000

    refactor: Move auth logic to template rather than layout.

--- debuglife-frontend/src/app/dashboard/layout.tsx
+++ debuglife-frontend/src/app/dashboard/layout.tsx
@@ -1,44 +1,12 @@
-"use client";
-
 // DashboardLayout.tsx
-import React, {useRef, useState, useEffect, ReactNode} from "react";
-import Navbar from "@/components/cms/navigation/NavBar";
-import Sidebar from "@/components/cms/navigation/Sidebar";
-import AuthGuard from "@/guards/AuthGuard";
-import {MilkdownProvider} from "@milkdown/react";
-import {ProsemirrorAdapterProvider} from "@prosemirror-adapter/react";
+import {ReactNode} from "react";
 
 const DashboardLayout = ({children}: {children: ReactNode}) => {
-    const [navbarHeight, setNavbarHeight] = useState(0);
-
-    const handleNavbarRef = (node: HTMLDivElement | null) => {
-        if (node) {
-            const {height} = node.getBoundingClientRect();
-            setNavbarHeight(height);
-        }
-    };
 
     return (
-        <AuthGuard>
-            <MilkdownProvider>
-                <ProsemirrorAdapterProvider>
-                    <div
-                        className="flex h-screen"
-                        style={
-                            {
-                                "--total-subtraction": `${navbarHeight}px`,
-                            } as React.CSSProperties
-                        }
-                    >
-                        <Sidebar />
-                        <div className="flex min-w-0 flex-1 flex-col">
-                            <Navbar ref={handleNavbarRef} />
-                            <main className="overflow-auto">{children}</main>
-                        </div>
-                    </div>
-                </ProsemirrorAdapterProvider>
-            </MilkdownProvider>
-        </AuthGuard>
+        <>
+        {children}
+        </>
     );
 };
 

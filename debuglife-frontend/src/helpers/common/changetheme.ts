3a0a7095887f6fc90bc5304b1274a3ef249128e9
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-05-20 14:00:00 +0000

    refactor: Move debuglife-frontend to seperate repo.

--- debuglife-frontend/src/helpers/common/changetheme.ts
+++
@@ -1,9 +0,0 @@
-export const toggleTheme = (): void => {
-    const htmlElement = document.documentElement;
-
-    if (htmlElement.getAttribute("data-theme") === "light") {
-        htmlElement.setAttribute("data-theme", "dark");
-    } else {
-        htmlElement.setAttribute("data-theme", "light");
-    }
-};
